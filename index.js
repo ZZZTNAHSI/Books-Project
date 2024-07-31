6db7a2f9449aa6a85fd86eebef169b0fa8e8f472
ZZZTnahsi <Ishantmahey1@gmail.com>
2024-07-31 08:34:48 -0500

    Initial git commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
