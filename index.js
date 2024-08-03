28811f4a51d26df45866ab622d0aa047c0374892
ZZZTnahsi <Ishantmahey1@gmail.com>
2024-08-03 12:11:49 -0500

    Everything is broken but can be fixed if you change anchor tag on book tile, but you lose hot anchor deco so choose wisely

--- index.js
+++ index.js
@@ -19,22 +19,45 @@ app.use(express.static("public"));
 let result = await db.query("SELECT * FROM books");
 result = result.rows;
 
-app.listen(port, () => {
-    console.log(`Website running of port ${port}`)
-});
-
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    result.forEach((book) => {
+        book.text = book.text.slice(0, 449) + '...';
+    });
+    // console.log(result)
+    res.render("index.ejs", {
+        bookPage: result
+    });
 });
 
 app.get("/addPage", (req, res) => {
     res.render("add.ejs");
 });
 
+app.get("/bookRev/:id", async (req, res) => {
+    console.log(req.params);
+    const id  = parseInt(req.params.id);
+    console.log("5000 IQ DIMENSIONAL BEING");
+    try {
+        const resultFull = await db.query("SELECT * FROM books WHERE id = ($1)", [id]);
+        res.render("book.ejs", {
+            bookPage: resultFull.rows
+        });
+    } catch (err) {
+        console.log(err);
+        res.redirect("/");
+    }
+});
+
 app.post("/add", async (req, res) => {
-    const info = req.body
+    const info = req.body;
     const rating = parseInt(info.rating);
     const dbAdd = [info.isbn, info.authName, info.title, info.date,rating, info.review];
     await db.query("INSERT INTO books(isbn, author, title, dateread, review, text) VALUES ($1, $2, $3, $4, $5, $6)", dbAdd);
+    result = await db.query("SELECT * FROM books");
     res.redirect("/");
-})
+});
+
+
+app.listen(port, () => {
+    console.log(`Website running of port ${port}`);
+});
